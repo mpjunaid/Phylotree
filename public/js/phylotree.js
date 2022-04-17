0a4ab0bf01e01188a00226ae5964dc6a8d2a5f33
Judith98 <j.s.houdijk@gmail.com>
2022-04-17 10:21:14 +0200

    Update phylotree.js

--- public/js/phylotree.js
+++ public/js/phylotree.js
@@ -2760,6 +2760,23 @@ ___namespace.extend(TreeRender.prototype, events);
 ___namespace.extend(TreeRender.prototype, menus);
 ___namespace.extend(TreeRender.prototype, opt);
 
+function resortChildren(comparator, start_node, filter) {
+  // ascending
+  this.nodes
+    .sum(function(d) {
+      return d.value;
+    })
+    .sort(comparator);
+
+  // if a tree is rendered in the DOM
+  if (this.display) {
+    this.display.update_layout(this.nodes);
+    this.display.update();
+  }
+
+  return this;
+}
+
 let Phylotree = class {
   constructor(nwk, options = {}) {
     this.newick_string = "";
@@ -2900,6 +2917,7 @@ Phylotree.prototype.isLeafNode = isLeafNode;
 Phylotree.prototype.hasBranchLengths = hasBranchLengths;
 Phylotree.prototype.scaleBranchLengths = scale;
 Phylotree.prototype.getNewick = getNewick;
+Phylotree.prototype.resortChildren = resortChildren;
 Phylotree.prototype.setBranchLength = setBranchLength;
 
 ___namespace.extend(Phylotree.prototype, node_operations);
